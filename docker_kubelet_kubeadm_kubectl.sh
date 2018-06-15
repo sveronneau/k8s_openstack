ff87f278dbaafd8a0ad1471060ec4bfc42b02bd8
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2018-06-15 09:55:30 -0400

    Create docker_kubelet_kubeadm_kubectl.sh

---
+++ docker_kubelet_kubeadm_kubectl.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get install -y docker.io
+sudo apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list 
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo sed -i "s/cgroup-driver=systemd/cgroup-driver=cgroupfs/g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
